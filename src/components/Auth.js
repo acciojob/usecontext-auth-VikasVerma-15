0edb741f2341036eb20c8b45a0869284fbc88b6c
Vikas Verma <vikasverma15@gmail.com>
2026-01-06 17:18:55 +0530

    update auth

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,3 +1,4 @@
+
 import React, { useContext } from "react";
 import { AuthContext } from "./AuthContext";
 
@@ -7,7 +8,10 @@ const Auth = () => {
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
       <h1>Click on the checkbox to get authenticated</h1>
-      <p>{isAuthenticated ? "You are authenticated" : "You are not authenticated"}</p>
+      {/* Add className 'authText' and exact text the test expects */}
+      <p className="authText">
+        {isAuthenticated ? "you are authenticated" : "you are not authenticated"}
+      </p>
       <label>
         <input type="checkbox" checked={isAuthenticated} onChange={toggleAuth} />
         I'm not a robot
