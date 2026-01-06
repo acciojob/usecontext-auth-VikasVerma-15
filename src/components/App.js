bae2338969f2d59b2f8c30ce1425f212db82f4ef
Vikas Verma <vikasverma15@gmail.com>
2026-01-06 17:14:03 +0530

    update app

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { AuthProvider } from "./AuthContext";
 import Auth from "./Auth";
-import './styles/App.css';
+import './../styles/App.css';
 
 function App() {
   return (
